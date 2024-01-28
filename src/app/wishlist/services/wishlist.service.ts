8e6212d4f5daaa93f5fee726aafa235e86c1fac4
Code <Lucas>
2024-01-28 16:43:07 -0300

    Populando os campos com os valores

--- src/app/wishlist/services/wishlist.service.ts
+++ src/app/wishlist/services/wishlist.service.ts
@@ -21,12 +21,23 @@ export class WishlistService {
     );
   }
 
-  create(record: WishList){
+  private create(record: WishList){
     return this.httpClient.post<WishList>(this.API, record )
     .pipe(first());
   }
 
+  save(record: WishList){
+    if(record.wishListId){
+      return this.update(record)
+    }
+    return this.create(record)
+  }
+
   loadByid( id: string ) {
     return this.httpClient.get<WishList>(`${this.API}/${id}`)
+  }
+
+  private update( record:  WishList ){
+    return this.httpClient.put<WishList>(`${this.API}/${record.wishListId}`, record).pipe(first());
   }
 }
--- src/app/wishlist/wishlist-form/wishlist-form.component.ts
+++ src/app/wishlist/wishlist-form/wishlist-form.component.ts
@@ -1,7 +1,9 @@
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ControlContainer, FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { WishList } from 'src/app/models/wishlist.model';
 import { WishlistService } from '../services/wishlist.service';
 
 @Component({
@@ -9,7 +11,7 @@ import { WishlistService } from '../services/wishlist.service';
   templateUrl: './wishlist-form.component.html',
   styleUrls: ['./wishlist-form.component.css']
 })
-export class WishlistFormComponent {
+export class WishlistFormComponent implements OnInit{
 
   form: FormGroup;
   cardTitle = "Cadastro de Items";
@@ -17,11 +19,20 @@ export class WishlistFormComponent {
   constructor(private formBuilder: FormBuilder,
     private snackbar: MatSnackBar,
     private location: Location,
-    private serviceWishlist : WishlistService){
+    private serviceWishlist : WishlistService,
+    private route : ActivatedRoute){
     this.form = this.formBuilder.group({
-      wishlistId : [''],
+      wishListId: [''],
       item: [''],
       link: ['']
+    });
+  }
+  ngOnInit(): void {
+    const wishList : WishList = this.route.snapshot.data['wishlist'];
+    this.form.setValue({
+      wishListId : wishList.wishListId,
+      item : wishList.item,
+      link : wishList.link
     });
   }
 
@@ -37,7 +48,7 @@ export class WishlistFormComponent {
   }
 
   save(){
-    this.serviceWishlist.create(this.form.value).subscribe(
+    this.serviceWishlist.save(this.form.value).subscribe(
       result => this.onSuccess(), error => {
         this.snackbar.open("Aconteceu um erro", "Fechar", {
           duration: 3000
